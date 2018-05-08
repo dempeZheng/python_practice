25de59edd951f5ef2b4d9854b297f75867b7ecca
dempezheng <dempezheng@kugou.net>
2018-05-08 18:27:50 +0800

    web项目实践

--- http/Http_Demo.py
+++ http/Http_Demo.py
@@ -4,3 +4,12 @@ import requests
 if __name__ == '__main__':
     r = requests.get('https://www.douban.com/')  # 豆瓣首页
     print(r.status_code)
+    # print(r.text)
+
+    r = requests.get('https://www.douban.com/', params={'q': 'python', 'cat': '1001'})
+    print(r.url)
+
+    r = requests.get(
+        'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json')
+    print(r.json())
+    # print(r.text)
