c6e5859e056dfeb5d4f5d86919dac393d05cb7e6
dempezheng <dempezheng@kugou.net>
2018-05-09 20:19:48 +0800

    orm->项目source目录

--- awesome-python3-webapp/www/test.py
+++ awesome-python3-webapp/www/test.py
@@ -1,10 +1,13 @@
 'another sql check code'
-from  models import User
-async def print_user():
-    data = await User.findAll()
-    for d in data:
-        print(d)
+import unittest
+
+class TestUnit(unittest.TestCase):
+
+    def test_init(self):
+        print("--------------")
+
 
 
 if __name__ == '__main__':
-    print_user()
+    unittest.main()
+
